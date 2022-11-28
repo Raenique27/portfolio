d2ed61bc1ff1436fb2ec007893d8ccca25d0006a
Raenique27 <raeniquewalters@gmail.com>
2022-11-28 12:14:50 -0500

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import About from './components/About';
 import Header from './components/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('About');
   return (
-    <div>
-      <Header></Header>
+    <Router>
+      <Header setCurrentPage={setCurrentPage} currentPage={currentPage}>
+
+      </Header>
       <main>
-        <About></About>
+        <Routes>
+          <Route path='/' element={<About/>} />
+          <Route path='/portfolio' element={<About/>}/>
+          <Route path='/About' element={<About/>} />
+        </Routes>      
       </main>
-    </div>
+    </Router>
   );
 }
 
