a9d1f81595f0cb6f76a9561f6212bd6e537912bf
Raenique27 <raeniquewalters@gmail.com>
2022-11-28 11:52:13 -0500

    set up Header component and imported to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import About from './components/About';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <main>
         <About></About>
       </main>
---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <section>
+                <h1>
+                    <a href='/'>Raenique Walters</a>
+                </h1>
+            </section>
+        </header>
+    );
+}
+
+export default Header;
